dee49c02a11730b229f4963a265bd6273b28ea99
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-10-09 20:38:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//css
 import './App.css';
 
+//react
+import { useCallback, useEffect, useState } from 'react';
+
+//data
+import { wordsList } from "./data/data"
+
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start"},
+  { id: 2, name: "game"},
+  { id: 3, name: "end"}
+]
+
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        {gameStage === "start" && <StartScreen />}
+        {gameStage === "game" && <Game />}
+        {gameStage === "end" && <GameOver />}
+      </h2>
     </div>
   );
 }
