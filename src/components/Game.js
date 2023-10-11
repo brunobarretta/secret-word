dcda41c9be66f9bcb5196e62a4848aa7c9c83503
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-10-11 07:31:40 -0300

    add css

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,32 @@
 import "./Game.css"
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
-    <div>Game</div>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <form>
+          <p>Tente advinhar uma letra da palavra:</p>
+          <input type="text" name="letter" maxLength={1} required/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>n,</span>
+      </div>
+    </div>
   )
 }
 
