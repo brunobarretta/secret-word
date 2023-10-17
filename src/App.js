09a1c95bba2f7d22bdcea7e065891f95f7788cf2
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-10-17 20:00:18 -0300

    Added game over page css

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ function App() {
             score={score}
           />
         )}
-        {gameStage === "end" && <GameOver retry={retry}/>}
+        {gameStage === "end" && <GameOver retry={retry} score={score} />}
       </h2>
     </div>
   );
